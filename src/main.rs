6fed77599c634d30a60722478e248454330893c5
Victor <123157458+veecore@users.noreply.github.com>
2026-05-06 12:52:51 +0100

    Save current project status

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use hook_inject::Library;
+use hook_inject::{Library, Process};
 
 fn main() {
     let library =
         Library::from_crate("/Users/tundeoladipupo/RustProjects/hook-inject/fixtures/agent")
-            .unwrap();
+            .unwrap()
+            .with_entrypoint(c"hook_inject_entry")
+            .with_data(c"/tmp/new_file");
+    // library
+    //     .inject_program("/Users/tundeoladipupo/RustProjects/hook-inject/zz_lab/misc/mango")
+    //     .unwrap();
     library
-        .inject_program("/Users/tundeoladipupo/RustProjects/hook-inject/main")
+        .inject_into_process(unsafe { Process::from_pid_unchecked(3626) })
         .unwrap();
 }
